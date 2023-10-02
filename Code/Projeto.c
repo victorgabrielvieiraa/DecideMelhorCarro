5784fba70562d4b12a5f7a76c1990e84477810b3
victorgabrielvieiraa <88777214+victorgabrielvieiraa@users.noreply.github.com>
2023-10-02 20:05:54 -0300

    fix no codigo na hora de exibir os carros

--- Code/Projeto.c
+++ Code/Projeto.c
@@ -33,8 +33,7 @@ void encontrarMelhorCarro(struct Carro carros[], int n, int maxPreco)
                 int escolherCarro = carros[i - 1].potencia + dp[i - 1][j - carros[i - 1].preco];
                 int naoEscolherCarro = dp[i - 1][j];
 
-                if (escolherCarro > naoEscolherCarro ||
-                    (escolherCarro == naoEscolherCarro && carros[i - 1].preco < carros[selecionados[i - 1]].preco))
+                if (escolherCarro > naoEscolherCarro)
                 {
                     dp[i][j] = escolherCarro;
                     selecionados[i] = i - 1; // Marca o índice do carro como selecionado
@@ -57,20 +56,17 @@ void encontrarMelhorCarro(struct Carro carros[], int n, int maxPreco)
     int j = maxPreco;
     int valorTotal = 0;
 
-    printf("Potência máxima: %d\n", potenciaMaxima);
     printf("Carros selecionados:\n");
 
     for (int i = n; i >= 1; i--)
     {
-        if (selecionados[i] != selecionados[i - 1])
+        if (dp[i][j] != dp[i - 1][j])
         {
             printf("%s (Potência: %d, Quilometragem: %d, Preço: %d)\n", carros[i - 1].nome, carros[i - 1].potencia, carros[i - 1].quilometragem, carros[i - 1].preco);
             valorTotal += carros[i - 1].preco;
             j -= carros[i - 1].preco;
         }
     }
-
-    printf("Valor total dos carros selecionados: %d\n", valorTotal);
 }
 
 int main()
